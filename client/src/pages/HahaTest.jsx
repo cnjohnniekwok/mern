e4910ec240200cb205f44187fd5aa2ed1794c006
cnjohnniekwok <30383300+cnjohnniekwok@users.noreply.github.com>
2021-04-01 14:56:39 +0800

    First upload of Mern client server

---
+++ client/src/pages/HahaTest.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class HahaTest extends Component {
+    render() {
+        return (
+            <div>
+                <p>In this page you'll just hahaha</p>
+            </div>
+        )
+    }
+}
+
+export default HahaTest;
---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+import MoviesList from './MoviesList'
+import MoviesInsert from './MoviesInsert'
+import MoviesUpdate from './MoviesUpdate'
+import HahaTest from './HahaTest'
+
+export { MoviesList, MoviesInsert, MoviesUpdate, HahaTest };
